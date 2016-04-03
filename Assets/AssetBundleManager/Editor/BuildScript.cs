b9382e6a85844aff7bf7878c36062a2a5f8255ac
Chatpong Suteesuksataporn <chatpong@progaming.co.th>
2016-04-03 16:29:18 +0700

    Hard code output path

--- Assets/AssetBundleManager/Editor/BuildScript.cs
+++ Assets/AssetBundleManager/Editor/BuildScript.cs
@@ -16,8 +16,9 @@ namespace AssetBundles
 		public static void BuildAssetBundles()
 		{
 			// Choose the output path according to the build target.
-			//string outputPath = Path.Combine(Utility.AssetBundlesOutputPath,  Utility.GetPlatformName());
-			//if (!Directory.Exists(outputPath) )
+			string outputPath = Path.Combine(Utility.AssetBundlesOutputPath,  Utility.GetPlatformName());
+            outputPath = "/home/ubuntu/Documents/Build/BundleSample";
+			if (!Directory.Exists(outputPath) )
 				//Directory.CreateDirectory (outputPath);
 	
 			//@TODO: use append hash... (Make sure pipeline works correctly with it.)
