0ea80cfa4e63a45ebb5338d41166b71a9e1c2b34
Chatpong Suteesuksataporn <chatpong@progaming.co.th>
2016-04-03 16:33:36 +0700

    Create folder

--- Assets/AssetBundleManager/Editor/BuildScript.cs
+++ Assets/AssetBundleManager/Editor/BuildScript.cs
@@ -18,8 +18,8 @@ namespace AssetBundles
 			// Choose the output path according to the build target.
 			//string outputPath = Path.Combine(Utility.AssetBundlesOutputPath,  Utility.GetPlatformName());
             string outputPath = "/home/ubuntu/Documents/Build/BundleSample";
-			//if (!Directory.Exists(outputPath) )
-				//Directory.CreateDirectory (outputPath);
+			if (!Directory.Exists(outputPath) )
+				Directory.CreateDirectory (outputPath);
 	
 			//@TODO: use append hash... (Make sure pipeline works correctly with it.)
 			//BuildPipeline.BuildAssetBundles (outputPath, BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
