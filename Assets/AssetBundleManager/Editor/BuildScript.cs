fe6d098d3149884f556aaa576aea567f432cad08
Chatpong Suteesuksataporn <chatpong@progaming.co.th>
2016-04-03 16:56:23 +0700

    Fix

--- Assets/AssetBundleManager/Editor/BuildScript.cs
+++ Assets/AssetBundleManager/Editor/BuildScript.cs
@@ -18,7 +18,7 @@ namespace AssetBundles
 			// Choose the output path according to the build target.
 			//string outputPath = Path.Combine(Utility.AssetBundlesOutputPath,  Utility.GetPlatformName());
             //string outputPath = "/home/ubuntu/Documents/Build/BundleSample";
-            string outputPath = Utility.AssetBundlesOutputPath + "/Linux";
+            string outputPath = Utility.AssetBundlesOutputPath + "/" + Utility.GetPlatformName();
             Debug.Log(Utility.AssetBundlesOutputPath);
             Debug.Log(Utility.GetPlatformName());
             
